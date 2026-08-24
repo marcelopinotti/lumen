ff865a758caedfd00625d46d6ae40635cf57902c
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-08-24 15:52:39 -0300

    Add CartaoController and CartaoService classes for managing Cartao entities

---
+++ cartoes/src/main/java/com/marcelo/cartoes/application/CartaoController.java
@@ -0,0 +1,47 @@
+package com.marcelo.cartoes.application;
+
+import com.marcelo.cartoes.domain.Cartao;
+import com.marcelo.cartoes.domain.ClienteCartao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/cartoes")
+public class CartaoController {
+
+    private final CartaoService cartaoService;
+    private final CartaoMapper cartaoMapper;
+    private final ClienteCartaoService clienteCartaoService;
+    private final ClienteCartaoMapper clienteCartaoMapper;
+
+    @PostMapping
+    public ResponseEntity<CartaoResponse> save(@RequestBody CartaoRequest request) {
+        var cartao = cartaoMapper.toModel(request);
+        var savedCartao = cartaoService.save(cartao);
+        var response = cartaoMapper.toResponse(savedCartao);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping(params = "renda")
+    public ResponseEntity<List<CartaoResponse>> renda(@RequestParam("renda") Long renda) {
+        var cartoes = cartaoService.renda(renda);
+        var response = cartoes.stream()
+                .map(cartaoMapper::toResponse)
+                .toList();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping(params = "cpf")
+    public ResponseEntity<List<ClienteCartaoResponse>> getdByCpf(@RequestParam("cpf") String cpf) {
+        List<ClienteCartao> lista = clienteCartaoService.findByCpf(cpf);
+        List<ClienteCartaoResponse> response = lista.stream()
+                .map(clienteCartaoMapper::toResponse)
+                .toList();
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ cartoes/src/main/java/com/marcelo/cartoes/application/CartaoService.java
@@ -0,0 +1,30 @@
+package com.marcelo.cartoes.application;
+
+import com.marcelo.cartoes.domain.Cartao;
+import com.marcelo.cartoes.infra.repository.CartaoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CartaoService {
+
+    private final CartaoRepository cartaoRepository;
+
+
+    @Transactional
+    public Cartao save(Cartao cartao) {
+        return cartaoRepository.save(cartao);
+    }
+
+    public List<Cartao> renda(Long renda) {
+        var rendaBigDecimal = BigDecimal.valueOf(renda);
+        return cartaoRepository.findByRendaLessThanEqual(rendaBigDecimal);
+    }
+
+}
