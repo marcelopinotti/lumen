ecb36dcbf963bd1789c60e06a591cac905833b2d
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-08-31 13:24:08 -0300

    refactor: Convert domain classes to records for improved immutability and clarity

--- avaliador/src/main/java/com/marcelo/avaliador/domain/CartaoCliente.java
+++ avaliador/src/main/java/com/marcelo/avaliador/domain/CartaoCliente.java
@@ -1,18 +1,12 @@
 package com.marcelo.avaliador.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.math.BigDecimal;
 
-@Getter
-@Setter
-public class CartaoCliente {
-
-    private String nome;
-
-    private String bandeira;
-
-    private BigDecimal limiteLiberado;
+public record CartaoCliente(
+        String nome,
+        String bandeira,
+        BigDecimal limiteLiberado
+) {
 
 }
+
