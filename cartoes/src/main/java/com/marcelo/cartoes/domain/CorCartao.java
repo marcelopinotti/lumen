da06f2c9c14e1672a3ff7c60fd1bf0f6720c046d
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-09-04 10:46:18 -0300

    feat: Update CorCartao enum to include minimum income and remove unique constraint from cpf in ClienteCartao

--- cartoes/src/main/java/com/marcelo/cartoes/domain/CorCartao.java
+++ cartoes/src/main/java/com/marcelo/cartoes/domain/CorCartao.java
@@ -1,14 +1,14 @@
 package com.marcelo.cartoes.domain;
 
+import java.math.BigDecimal;
+
 public enum CorCartao {
-    PRATA("Prata"),
-    OURO("Ouro"),
-    PLATINA("Platina"),
-    BLACK("Black");
-
-    private String nome;
-
-    CorCartao(String nome) {
-        this.nome = nome;
-    }
+    PRATA("Prata", "0"),
+    OURO("Ouro", "5000"),
+    PLATINA("Platina", "10000"),
+    BLACK("Black", "20000");
+    private final String nome;
+    private final BigDecimal rendaMinima;
+    CorCartao(String nome, String rendaMinima) { this.nome = nome; this.rendaMinima = new BigDecimal(rendaMinima); }
+    public BigDecimal rendaMinima() { return rendaMinima; }
 }
